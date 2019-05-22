7d8efdf4b697fd589ee66580e537e79ae97442b7
SteefanContractor <stefan.contractor@gmail.com>
2019-05-22 15:04:26 +1000

    Modify processRealtimeSSTdata.R so sst data is now saved as raster to aid with mapping with leaflet and data is saved in the app data dir

--- processRealtimeSSTdata.R
+++ processRealtimeSSTdata.R
@@ -4,50 +4,49 @@
 library(ncdf4)
 library(lubridate)
 library(maps)
+library(dplyr)
+library(raster) #
 
 # change local to True when developing locally
 local = system("uname -n", intern = T) == "matht250"#T
 
 if (local) {
-  basePath <- "~/Documents/GIT_REPOS/NSW-IMOS_data_viz_app/data/"#"~/ownCloud/Working_Directory/Postdoc-SchoolOfMathsStats/Scripts/Simple_shiny_Climatology_dashboard_app/data/"
+  basePath <- "~/Documents/GIT_REPOS/NSW-IMOS_data_viz_app/"#"~/ownCloud/Working_Directory/Postdoc-SchoolOfMathsStats/Scripts/Simple_shiny_Climatology_dashboard_app/data/"
   #"~/sci-maths-ocean/shared/PEOPLE/Steefan/climatology/data/"
 } else {
-  basePath <- "./data/"
+  basePath <- "./"
 }
 
-files <- list.files(basePath, pattern = glob2rx("*.nc"))
+files <- list.files(paste0(basePath,"data/"), pattern = glob2rx("*.nc"))
 date_times <- ymd_hms(substr(files, 1,14))
 df <- data.frame(date_time = date_times, filename = files)
 df <- arrange(df, desc(date_time))
 
 # read in first nc
-nc <- nc_open(paste0(basePath,df$filename[1]))
+nc <- nc_open(paste0(basePath,"data/",df$filename[1]))
 lon <- ncvar_get(nc, "lon")
 lat <- rev(ncvar_get(nc, "lat"))
 qflag <- ncvar_get(nc, "quality_level")
-qflag <- qflag[,length(lat):1]
 sst <- ncvar_get(nc, "sea_surface_temperature")
-sst <- sst[,dim(sst)[2]:1]
 sst[which(qflag < 4)] <- NA
-qflag[which(qflag < 4)] <- NA
 nc_close(nc)
 
 # Now systematically go through previous sst fields and fill in gaps
-for (t in 2:48){
-  nc <- nc_open(paste0(basePath,df$filename[t]))
+for (t in 6:48){
+  # t = 5
+  nc <- nc_open(paste0(basePath,"data/",df$filename[t]))
   qflag_prev <- ncvar_get(nc, "quality_level")
-  qflag_prev <- qflag[,length(lat):1]
   sst_prev <- ncvar_get(nc, "sea_surface_temperature")
-  sst_prev <- sst_prev[,dim(sst)[2]:1]
   sst_prev[which(qflag_prev < 4)] <- NA
-  # qflag_prev[which(qflag_prev < 4)] <- NA
   nc_close(nc)
   sst[which(is.na(sst) & qflag_prev >= 4)] <- sst_prev[which(is.na(sst) & qflag_prev >= 4)]
-  qflag[which(is.na(sst) & qflag_prev >= 4)] <- qflag_prev[which(is.na(sst) & qflag_prev >= 4)]
 }
-qflag[is.na(sst)] <- NA
-save(lon, lat, sst, qflag, file = paste0(basePath,"latestSST.Rdata"))
+# convert to raster
+sst <- raster(t(sst), xmn=min(lon), xmx=max(lon), ymn=min(lat), ymx=max(lat), 
+              crs="+proj=longlat +ellps=WGS84 +datum=WGS84 +no_defs")
 
+# save data
+save(sst, file = paste0(basePath,"Simple_shiny_Climatology_dashboard_app/data/latestSST.Rdata"))
 
 # image(lon, lat, sst)
 # image(lon, lat, qflag)
@@ -57,5 +56,5 @@ save(lon, lat, sst, qflag, file = paste0(basePath,"latestSST.Rdata"))
 # 
 # sst.df <- data.frame(expand.grid(lon,lat), sst = c(sst))
 # colnames(sst.df) <- c("lon", "lat", "sst")
-# plot_ly(data = sst.df, x = lon, y = lat, z = t(sst), type = "contour")
+# plot_ly(data = sst.df, x = lon, y = lat, z = t(sst), type = "heatmap")
 # image(lon, lat, qflag)
