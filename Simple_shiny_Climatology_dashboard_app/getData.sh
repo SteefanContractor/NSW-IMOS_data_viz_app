25ef461b5988b487d190961ad779264a358e5292
SteefanContractor <stefan.contractor@gmail.com>
2019-11-20 17:46:59 +1100

    update download script so it downloads daily L$ SST product and daily OC product for 4 weeks

--- Simple_shiny_Climatology_dashboard_app/getData.sh
+++ Simple_shiny_Climatology_dashboard_app/getData.sh
@@ -6,32 +6,28 @@ cd data/SST
 touch lastwatch
 
 # delete old files
-if [ -f months.txt ]; then rm months.txt; fi
 if [ -f fnames.txt ]; then rm fnames.txt; fi
-if [ -f fnames_lastmon.txt ]; then rm fnames_lastmon.txt; fi
+if [ -f fnames_lastyear.txt ]; then rm fnames_lastyear.txt; fi
+thisyear=`date -u +%Y`
+lastyear=$(( thisyear - 1 ))
 
-# Get the latest two available months
-wget -q -O - http://opendap.bom.gov.au:8080/thredds/catalog/abom_imos_ghrsst_archive-1/v02.0fv03/Continental/L3C-01hour/ABOM-L3C_GHRSST-SSTskin-AxIH08/2019/catalog.html | grep -oP  "(?<=href=')[0-9][0-9](?=/catalog.html')" | sort -nr >> months.txt
-thismon=`head -n 1 months.txt`
-lastmon=`head -n 2 months.txt | tail -n 1`
-
-# Get the names of latest 48 files of hourly data from this month
-wget -q -O - http://opendap.bom.gov.au:8080/thredds/catalog/abom_imos_ghrsst_archive-1/v02.0fv03/Continental/L3C-01hour/ABOM-L3C_GHRSST-SSTskin-AxIH08/2019/"$thismon"/catalog.html | grep -oP  "(?<=ABOM-L3C_GHRSST-SSTskin-AxIH08/2019/"$thismon"/).*.nc(?='><tt>)" | tail -n 48 >> fnames.txt
+# Get the names of latest 28 files of daily data from RAMSSA
+wget -q -O - http://thredds.aodn.org.au/thredds/catalog/IMOS/SRS/SST/ghrsst/L4/RAMSSA/"$thisyear"/catalog.html | grep -oP  "(?<=<tt>).*.nc(?=</tt>)" | tail -n 28 >> fnames.txt
 
 # Download files in fnames.txt
 for f in `cat fnames.txt`; do
-	wget -q -nc -A.nc http://opendap.bom.gov.au:8080/thredds/fileServer/abom_imos_ghrsst_archive-1/v02.0fv03/Continental/L3C-01hour/ABOM-L3C_GHRSST-SSTskin-AxIH08/2019/"$thismon"/"$f"
+	wget -q -nc -A.nc http://thredds.aodn.org.au/thredds/fileServer/IMOS/SRS/SST/ghrsst/L4/RAMSSA/"$thisyear"/"$f"
 done
 
-# If there aren't 48 files in the current month then get the remaining files from last month
+# If there aren't 28 files in the current year then get the remaining files from last year
 totfiles=`cat fnames.txt | wc -l`
-if [ $totfiles -lt 48 ]; then
-	remainfiles=$(( 48 - $totfiles ))		
-	wget -q -O - http://opendap.bom.gov.au:8080/thredds/catalog/abom_imos_ghrsst_archive-1/v02.0fv03/Continental/L3C-01hour/ABOM-L3C_GHRSST-SSTskin-AxIH08/2019/"$lastmon"/catalog.html | grep -oP  "(?<=ABOM-L3C_GHRSST-SSTskin-AxIH08/2019/"$lastmon"/).*.nc(?='><tt>)" | tail -n "$remainfiles" > fnames_lastmon.txt
-	for f in `cat fnames_lastmon.txt`; do
-        	wget -q -nc -A.nc http://opendap.bom.gov.au:8080/thredds/fileServer/abom_imos_ghrsst_archive-1/v02.0fv03/Continental/L3C-01hour/ABOM-L3C_GHRSST-SSTskin-AxIH08/2019/"$lastmon"/"$f"
+if [ $totfiles -lt 28 ]; then
+	remainfiles=$(( 28 - $totfiles ))		
+	wget -q -O - http://thredds.aodn.org.au/thredds/catalog/IMOS/SRS/SST/ghrsst/L4/RAMSSA/"$lastyear"/catalog.html | grep -oP  "(?<=<tt>).*.nc(?=</tt>)" | tail -n "$remainfiles" > fnames_lastyear.txt
+	for f in `cat fnames_lastyear.txt`; do
+        	wget -q -nc -A.nc http://thredds.aodn.org.au/thredds/fileServer/IMOS/SRS/SST/ghrsst/L4/RAMSSA/"$lastyear"/"$f"
 	done
-	cat fnames_lastmon.txt >> fnames.txt
+	cat fnames_lastyear.txt >> fnames.txt
 fi
 
 # Remove old files or nc files not in fnames.txt
@@ -52,10 +48,7 @@ fi
 
 rm lastwatch
 
-###############################################################
-# Download HF Radar data for the latest day for NEWC and COFH
-###############################################################
-
+# save dates for HFRadar data
 latestdate=`ls *.nc | cut -c1-8 | tail -n 1`
 year=${latestdate:0:4}
 month=${latestdate:4:2}
@@ -64,14 +57,59 @@ latesttime=`ls *.nc | cut -c9-14 | tail -n 1`
 latesthour=${latesttime:0:2}
 latesthourminus1=$(( latesthour - 1 ))
 latesthourminus2=$(( latesthour - 2 ))
+
+cd ../
+
+###############################################################
+# Download oceancolour data for the same month as SST above
+###############################################################
+
+
+if [ ! -d CHL_OC3 ]; then mkdir CHL_OC3; fi
+cd CHL_OC3
+# a file to distinguish old from new files
+touch lastwatch
+
+cat ../SST/fnames.txt | cut -c 1-8 > dates.txt
+
+for d in `cat dates.txt`; do
+  y=`echo "$d" | cut -c1-4`
+  m=`echo "$d" | cut -c5-6`
+  d=`echo "$d" | cut -c7-8`
+  wget -q -nc -A.nc http://thredds.aodn.org.au/thredds/fileServer/IMOS/SRS/OC/gridded/aqua/P1D/"$y"/"$m"/A.P1D."$y$m$d"T000000Z.aust.chl_oc3.nc
+  echo A.P1D."$y$m$d"T000000Z.aust.chl_oc3.nc >> fnames.txt
+done  
+
+# Remove old files or nc files not in fnames.txt
+rm `ls *.nc | grep -v -f fnames.txt`
+
+# file with list of new nc files
+find -cnewer lastwatch -name "*.nc" -exec basename {} \; > newfiles.txt
+
+# Use CDO to subset to the region of interest
+if [ -s newfiles.txt ]; then 
+	for f in `cat newfiles.txt`; do
+		fname="${f%.*}"
+		cdo sellonlatbox,149.5,155.5,-28,-37.5 "$f" "$fname"_Aus-NSW.nc
+		cdo copy "$fname"_Aus-NSW.nc "$f"
+		rm "$fname"_Aus-NSW.nc
+	done
+fi
+
+rm lastwatch
+
+cd ../
+
+###############################################################
+# Download HF Radar data for the latest day for NEWC and COFH
+###############################################################
+
 #if [ 10#$latesthour -eq 0 ]; then 
 #	latesthourminus1=23
 #	day=`printf "%02d" $(( 10#$day - 1 ))`
 #else
 #	latesthourminus1=`printf "%02d" $(( 10#$latesthour - 1 ))`
 #fi
-
-cd ../
 
 if [ ! -d HFRadar ]; then mkdir HFRadar; fi
 cd HFRadar
